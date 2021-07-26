dbb195d7e1802345f8c9a8e6b24f349df391bae9
szymo <szymon.basiul@wp.pl>
2021-07-26 22:42:36 +0200

    More specified concepts, basic elements added.

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,12 @@ function App() {
   const text1 = "Semper fidelis"
   const text2 = "Bzdura"
 
+  const default_text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, minima ullam? Tempora minima, dolorem eligendi maiores expedita rem quod dignissimos natus! Nisi sed, molestiae voluptatum in minima accusamus autem fugit."
   const user_face = require("./img/default_user.jpg").default;
   const user_face2 = require("./img/default_user.jpg").default;
 
   return (
     <main>
-
-      <div className="header">
-      <div className="intro">
-      {intro}
-      </div>
-      </div>
       <div className="personal-container">
         <div className="person-one">
           <div className="photo">
@@ -24,31 +19,33 @@ function App() {
 
           </div>
           <div className="personal-info">
-
+            {default_text}
           </div>
           <div className="personal-bar">
+            bar
 
           </div>
         </div>
+        <div className="logotype">
+          logo
+        </div>
+
+
         <div className="person-two">
           <div className="photo">
           <img className="person-img" src={user_face2}/>
           </div>
           <div className="personal-info">
-
+            {default_text}
           </div>
           <div className="personal-bar">
-            
+            bar
           </div>
         </div>
 
-
-
-
-
-        
-        
-
+      </div>
+      <div className="footer">
+        logo/contact us/ yt-fb-tweeter
       </div>
     
 
--- src/dist/App.js
+++ src/dist/App.js
@@ -6,21 +6,22 @@ function App() {
     var intro = "BoSo Company";
     var text1 = "Semper fidelis";
     var text2 = "Bzdura";
+    var default_text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, minima ullam? Tempora minima, dolorem eligendi maiores expedita rem quod dignissimos natus! Nisi sed, molestiae voluptatum in minima accusamus autem fugit.";
     var user_face = require("./img/default_user.jpg")["default"];
     var user_face2 = require("./img/default_user.jpg")["default"];
     return (react_1["default"].createElement("main", null,
-        react_1["default"].createElement("div", { className: "header" },
-            react_1["default"].createElement("div", { className: "intro" }, intro)),
         react_1["default"].createElement("div", { className: "personal-container" },
             react_1["default"].createElement("div", { className: "person-one" },
                 react_1["default"].createElement("div", { className: "photo" },
                     react_1["default"].createElement("img", { className: "person-img", src: user_face })),
-                react_1["default"].createElement("div", { className: "personal-info" }),
-                react_1["default"].createElement("div", { className: "personal-bar" })),
+                react_1["default"].createElement("div", { className: "personal-info" }, default_text),
+                react_1["default"].createElement("div", { className: "personal-bar" }, "bar")),
+            react_1["default"].createElement("div", { className: "logotype" }, "logo"),
             react_1["default"].createElement("div", { className: "person-two" },
                 react_1["default"].createElement("div", { className: "photo" },
                     react_1["default"].createElement("img", { className: "person-img", src: user_face2 })),
-                react_1["default"].createElement("div", { className: "personal-info" }),
-                react_1["default"].createElement("div", { className: "personal-bar" })))));
+                react_1["default"].createElement("div", { className: "personal-info" }, default_text),
+                react_1["default"].createElement("div", { className: "personal-bar" }, "bar"))),
+        react_1["default"].createElement("div", { className: "footer" }, "logo/contact us/ yt-fb-tweeter")));
 }
 exports["default"] = App;
